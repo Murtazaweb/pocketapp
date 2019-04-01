cbc378e8c387f69bcb9a2ee8df50158d76779a93
Murtazaweb <beast.onmode20@gmail.com>
2019-04-01 13:00:18 +0500

    adding mongoLAB url for herokuconfiguration

--- server/src/database.js
+++ server/src/database.js
@@ -1,25 +1,15 @@
-import {MongoClient} from 'mongodb'
+import { MongoClient } from "mongodb";
 
-const URL = 'mongodb://localhost:27017/chatapp';
+//const URL = 'mongodb://localhost:27017/chatapp';
 
-
-export default class Database{
-
-	connect(){
-
-
-		return new Promise((resolve, reject) => {
-
-			MongoClient.connect(URL, (err, db) => {
-				
-				return err ? reject(err) : resolve(db);
-
-			});
-
-
-		});
-
-
-
-	}
+const URL =
+  "mongodb://murtaza:murtaza111@ds051524.mlab.com:51524/heroku_8ppk1qtg";
+export default class Database {
+  connect() {
+    return new Promise((resolve, reject) => {
+      MongoClient.connect(URL, (err, db) => {
+        return err ? reject(err) : resolve(db);
+      });
+    });
+  }
 }
